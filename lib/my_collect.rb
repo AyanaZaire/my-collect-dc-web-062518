1f520004c08d0469ef6ef6e15482aff7c11d5198
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 22:23:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,3 +8,14 @@ def my_collect(collection)
   end
   collection
 end
+
+collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+def my_collect(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+end
