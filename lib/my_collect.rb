bb79fabf11169a76acf23b0563cf275983ecf551
AyanaZaire <ayanazaire@gmail.com>
2018-06-14 22:22:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
-def my_collect(array) do |name|
-  name.split(" ").first
+def my_collect(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
-
